77dbda90d488caeafa7f86280741d610ea0941af
Zachary Lacey <zlacey1234@gmail.com>
2019-09-14 05:40:49 -0400

    Test
    Testing again

---
+++ AddPaths.m
@@ -0,0 +1,8 @@
+% AUTHORS: PABLO GARCIA BELTRAN, ZACHARY LACEY
+% AFFILIATION : UNIVERSITY OF MARYLAND
+% EMAIL : pgarciab@umd.edu
+%         zlacey@umd.edu
+%         zlacey1234@gmail.com
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath('./TestingDataSet');
