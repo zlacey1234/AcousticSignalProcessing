61fd63e86985b9cd18c43cb277410acb94ea5582
PabloCIV <pablopgb.pgb@gmail.com>
2019-09-18 20:04:37 -0400

    Plotting and Testing Concepts
    Added some code to plot spectrograms and files. Also isolates silence regions in the audio.

---
+++ read_and_plot.m
@@ -0,0 +1,56 @@
+[~, Fs] = audioread('./audio/parrot/parrot1.wav', [1 10]);
+start_t = 1;
+end_t = 60;
+[y, Fs] = audioread('./audio/parrot/parrot1.wav', Fs * [start_t end_t]);
+y = y(:,1); % get rid of seconds channel
+y = y ./ max(abs(y)); % normalize audio
+T = 1/Fs;
+N = (length(y) * T) - T;
+t = 0: T: N;
+
+% Plot audio in time domain
+plot(t, y); xlabel('Seconds'); ylabel('Amplitude');
+
+% Plot audio's spectrogram
+figure;
+spectrogram(y(:,1), 128, 120, 128, Fs, 'yaxis');
+
+% Silence analysis
+w_len = 50e-3 * Fs;
+segs = buffer(y, w_len);
+
+win = hann(w_len, 'periodic');
+sig_energy = sum(segs.^2, 1) / w_len;
+centroids = spectralCentroid(segs, Fs, 'Window', win, 'OverlapLength', 0);
+
+T_E = mean(sig_energy) / 2;
+T_C = 7000;
+is_roi = (sig_energy >= T_E) & (centroids <= T_C);
+
+CC = repmat(centroids, w_len, 1);
+CC = CC(:);
+EE = repmat(sig_energy, w_len, 1);
+EE = EE(:);
+flags_2 = repmat(is_roi, w_len, 1);
+flags_2 = flags_2(:);
+
+figure
+
+subplot(3, 1, 1);
+plot(t, CC(1: length(y)), t, repmat(T_C, 1, length(t)), 'LineWidth', 2);
+xlabel('Time (s)'); ylabel('Normalized Centroid');
+legend('Centroid', 'Threshold'); title('Spectral Centroid');
+grid on
+
+subplot(3, 1, 2);
+plot(t, EE(1: length(y)), t, repmat(T_E, 1, length(t)), 'LineWidth', 2);
+xlabel('Time (s)'); ylabel('Normalized Energy');
+legend('Energy', 'Threshold'); title('Window Energy');
+grid on
+
+subplot(3, 1, 3);
+plot(t, y, t, flags_2(1: length(y)), 'LineWidth', 2);
+xlabel('Time (s)'); ylabel('Audio');
+legend('Audio', 'ROI'); title('Audio');
+grid on
+ylim([-1 1.1])
