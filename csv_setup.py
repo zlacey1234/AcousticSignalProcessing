e63086984579317ff653e69bffba12d8dec8753a
PabloCIV <pablopgb.pgb@gmail.com>
2019-10-07 20:26:10 -0400

    Quick Additions For Python 2

--- csv_setup.py
+++ csv_setup.py
@@ -2,7 +2,7 @@ import soundfile as sf
 import os, csv
 
 def main(dir_name):
-    csv_data = [['path', 'label', 'length']]
+    csv_data = [['path', 'label', 'length', 'Fs']]
     dir = ''
     files = []
     for (dir, _, files) in os.walk(dir_name):
@@ -10,7 +10,7 @@ def main(dir_name):
             for file in files:
                 file_path = os.path.join(os.getcwd(), dir, file)
                 f = sf.SoundFile(file_path)
-                csv_data.append([file_path, dir[dir.index('\\') + 1:], len(f) / f.samplerate])
+                csv_data.append([file_path, dir[dir.index('\\') + 1:], len(f) / f.samplerate, f.samplerate])
     with open(os.path.join(dir_name, 'audio_data.csv'), 'wb+') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerows(csv_data)
